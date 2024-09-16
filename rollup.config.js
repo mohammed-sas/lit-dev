1018dc0108bd3168b9cd8e78fb29164f4a6b925e
Mohammed <mohammed@facilio.com>
2024-09-16 12:49:50 +0530

    scaffolding

--- rollup.config.js
+++ rollup.config.js
@@ -6,16 +6,31 @@ import { terser } from "rollup-plugin-terser";
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
-export default {
-  input: "src/components/fc-icon.js",
-  output: {
-    file: "dist/fc-icon.js",
-    format: "esm", // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: true,
+export default [
+  {
+    input: "src/components/fc-icon.js",
+    output: {
+      file: "dist/fc-icon.js",
+      format: "esm", // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(), // tells Rollup how to find date-fns in node_modules
+      commonjs(), // converts date-fns to ES modules
+      production && terser(), // minify, but only in production
+    ],
   },
-  plugins: [
-    resolve(), // tells Rollup how to find date-fns in node_modules
-    commonjs(), // converts date-fns to ES modules
-    production && terser(), // minify, but only in production
-  ],
-};
+  {
+    input: "src/components/fc-image.js",
+    output: {
+      file: "dist/fc-image.js",
+      format: "esm", // immediately-invoked function expression — suitable for <script> tags
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(), // tells Rollup how to find date-fns in node_modules
+      commonjs(), // converts date-fns to ES modules
+      production && terser(), // minify, but only in production
+    ],
+  },
+];
