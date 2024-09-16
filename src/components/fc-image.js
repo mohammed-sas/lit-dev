c2f983533fc87d4b563a0d874d45fca220e43ffc
Mohammed <mohammed@facilio.com>
2024-09-16 13:02:09 +0530

    feat:image

--- src/components/fc-image.js
+++ src/components/fc-image.js
@@ -4,7 +4,8 @@ export class FcImage extends LitElement {
   static get properties() {
     return {
       name: { attribute: true },
-      size: { attribute: true },
+      height: { attribute: true },
+      width: { attribute: true },
       color: { attribute: true },
       _baseURL: { state: true },
       _sym: { state: true },
@@ -18,19 +19,12 @@ export class FcImage extends LitElement {
   updated() {
     this.loadIcon();
   }
-  async getSvgContent(url) {
-    try {
-      let rsp = await fetch(url);
-      let content = rsp.text();
-      return content;
-    } catch (e) {}
-  }
+
   async loadIcon() {
     let cdnUrl = "https://icons.facilio.com/icons/svg/";
     let baseUrl = window[this.sym]?.baseUrl ? window[this.sym].baseURL : cdnUrl;
     const url = `${baseUrl}images/${this.name}.webp`;
     this._url = url;
-    this._svgContent = await this.getSvgContent(this._url);
   }
   render() {
     return html` <img
